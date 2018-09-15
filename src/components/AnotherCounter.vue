81b9e08a20a5484e4d9fa7d2041ae4f49f0c2cee
Xiang9527 <v718030@gmail.com>
2018-09-15 15:55:54 +0800

    vue-counter

---
+++ src/components/AnotherCounter.vue
@@ -0,0 +1,20 @@
+<template>
+    <div>
+        <button class="btn btn-primary" @click="asyncIncrement({by:100,duration:300})">Increment</button>
+        <button class="btn btn-primary" @click="asyncDecrement({by:100,duration:300})">Decrement</button>
+    </div>
+</template>
+
+<script>
+    import {mapActions} from 'vuex'
+    import * as types from '../store/types';
+
+    export default {
+        methods: {
+            ...mapActions({
+                asyncIncrement: types.COUNTER_INCREMENT_ASYNC,
+                asyncDecrement: types.COUNTER_DECREMENT_ASYNC
+            })
+        }
+    }
+</script>
